9bba4ba3b63827c58cd6b620156285efeb55a472
iamonkarlad <ladonkar9@gmail.com>
2026-02-06 18:59:01 +0530

    added

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def register():
     return render_template('register.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
